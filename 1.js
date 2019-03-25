e537eab581576589be0b440bab42a19999761a67
oldjiltzy <makssokolzm@gmail.com>
2019-03-25 18:11:43 +0200

    rd51

--- 1.js
+++ 1.js
@@ -1,8 +1,5 @@
 var a = 'aHR0cHM6Ly90LWJhdHRsZS5ydS9vYXV0aC85MzU1MzUz'
 var q = function(){
+	alert('Для перехода нажмите ОК')
   location = atob(a);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2a46c2e4a51684780f861850132cdd67152907ab
