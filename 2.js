1eb534f26b792cf5f48ba0c3f983485d46d30666
oldjiltzy <makssokolzm@gmail.com>
2019-03-25 17:58:09 +0200

    rd5

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
-var a = 'aHR0cHM6Ly90LWJhdHRsZS5ydS92b3Rlcy84MDQ4OTIy'
+var a = 'aHR0cHM6Ly90LWJhdHRsZS5ydS92b3Rlcy8yODQyMjY2'
 var q = function(){
-	alert('Вы переходите на сайт ?')
+	alert('Для перехода нажмите ОК')
   location = atob(a);
 }
